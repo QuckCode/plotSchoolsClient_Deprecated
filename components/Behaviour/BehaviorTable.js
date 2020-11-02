8f2a7fcca9166d1ba19adfc87aa913e1c59982ad
CodesDaniel <mailidanielifechukwu@gmail.com>
2020-11-02 19:31:05 +0100

    bahaviour component

---
+++ components/Behaviour/BehaviorTable.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import { Table, Button , Icon, Avatar} from 'antd';
+import { useAppState } from '../shared/AppProvider';
+
+
+const BehaviourTable= ({behaviour})=>{
+   const [tableHeight, setTableHeight] = React.useState(0)
+   const [state] = useAppState()
+   const columns = [
+    {
+      title: 'Department',
+      width: state.mobile?100:150,
+      dataIndex: 'name',
+      key: 'name',
+    },
+    {
+      title: 'Action',
+      key: 'operation',
+      width: state.mobile?100:150,
+      render: () =>(
+        <Button  type="primary" htmlType="submit">
+         <Icon type="edit" />
+         Edit
+      </Button>
+      )
+    },
+  ];
+  
+  React.useEffect(() => {
+    setTableHeight(window.innerHeight-280)
+  }, []);
+   return(
+     
+    <Table 
+     columns={columns}   
+     loading={test.loading} 
+     bordered
+     size="default"
+       pagination={true}  dataSource={test.tests} scroll={{ x: state.mobile?200:300, y: tableHeight }} />
+   )
+}
+  
+ 
+ 
+export default TestTable ;
---
+++ components/Behaviour/RegisterBehaviourType.js
@@ -0,0 +1,47 @@
+import {Button,Form,Input, Modal, Icon, InputNumber} from 'antd';
+import { school } from '../../redux/varables';
+
+const FormItem = Form.Item;
+
+
+const  RegistrationBehaviourType = ({form, createTest, loading})=> {
+    const formItemLayout = {labelCol: { xs: { span: 24 },sm: { span: 8 } }, wrapperCol: {xs: { span: 24 },sm: { span: 16 }} };
+    const tailFormItemLayout = { wrapperCol: { xs: { span: 24,   offset: 0 }, sm: {span: 16, offset: 8} } };
+    console.log(form)
+    return (
+      <div className="p-4">
+      <Form onSubmit= {(e)=>{
+         e.preventDefault();
+          form.validateFields((err, values) => {
+            if (!err) {
+               createTest({...values, school:school})
+               .then(()=>{
+                  form.resetFields()
+                  Modal.success({
+                    title:"Create new  successfully"
+                  })
+                  form.resetFields(['name'])
+               })
+               .catch(err=>{
+                Modal.error({
+                    title:err.title,
+                    content:err.message
+                })
+               })
+            }
+          });
+      }}>
+        <FormItem {...formItemLayout} label="Test/ Exam">
+        {form.getFieldDecorator('name', {rules: [ {required: true,message: 'Please Input  Department'}] })(<Input />)}
+        </FormItem>
+        <FormItem {...tailFormItemLayout}>
+          <Button  disabled={loading} type="primary" htmlType="submit">
+          { loading?   <Icon type="loading" />   : (<> </>) }    Submit
+          </Button>
+        </FormItem>
+      </Form>
+      </div>
+    );
+  }
+
+export default  Form.create()(RegistrationBehaviourType);
