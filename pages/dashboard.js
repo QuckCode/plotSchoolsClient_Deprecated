f7fcea6d581d03bfb8571ed426bcfee33451e877
CodesDaniel <mailidanielifechukwu@gmail.com>
2020-11-03 11:02:34 +0100

    chanages

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,8 +1,31 @@
 import  React from 'react'
 import Head from 'next/head';
-const DashboardPage = () => (
-  <>     
+import Overview from '../components/Overview';
+import { wrapper } from '../redux/store';
+const DashboardPage = ({student, classes,scratchCard, staff}) => {
+  console.log(staff)
+  return  (
+  
+  <>    
+    <Overview student={student} classes={classes} scratchCard={scratchCard} staff={staff}/> 
   </>
-);
+)
+  }
+
+export const getServerSideProps = wrapper.getServerSideProps(
+  async ({ store }) => {
+    
+    return {
+      props:{
+         student:100,
+         classes:5,
+         scratchCard:100,
+         staff:10
+      }
+    }
+  }
+)
+
+
 
 export default DashboardPage;
