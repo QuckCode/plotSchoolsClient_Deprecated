33161864fc1b3b44c5e09406e006e56c4faf1b7c
CodesDaniel <mailidanielifechukwu@gmail.com>
2021-01-23 17:43:08 +0100

    changes

--- components/Arm/RegisterArm.js
+++ components/Arm/RegisterArm.js
@@ -9,7 +9,6 @@ const  RegistrationArm = ({form, classes, createArm, arm})=> {
     const formItemLayout = {labelCol: { xs: { span: 24 },sm: { span: 8 } }, wrapperCol: {xs: { span: 24 },sm: { span: 16 }} };
     const tailFormItemLayout = { wrapperCol: { xs: { span: 24,   offset: 0 }, sm: {span: 16, offset: 8} } };
      const {loading} =arm
-     console.log(loading)
     return (
       <div className="p-4">
       <Form onSubmit= {(e)=>{
