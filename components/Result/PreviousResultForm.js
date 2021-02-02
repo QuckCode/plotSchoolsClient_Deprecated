9508baa3c79ff243df7e4d5805f3a2bf9911eda2
CodesDaniel <mailidanielifechukwu@gmail.com>
2021-02-02 19:17:42 +0100

    previous result form

---
+++ components/Result/PreviousResultForm.js
@@ -0,0 +1,74 @@
+import React, { useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { Form , Button, Input, Icon, Select} from 'antd';
+const FormItem = Form.Item
+const Option = Select.Option
+import {useState} from 'react'
+ 
+const PreviousResultForm = ({form, previousSections}) => {
+ 
+ const [loading, setLoading] = useState(false) 
+ const [sections, setSections] = useState([])
+ const formItemLayout = {labelCol: { xs: { span: 24 },sm: { span: 8 } }, wrapperCol: {xs: { span: 24 },sm: { span: 16 }} };
+ const tailFormItemLayout = { wrapperCol: { xs: { span: 24,   offset: 0 }, sm: {span: 16, offset: 8} } };
+ const {validateFields, getFieldDecorator} = form
+
+ useEffect(()=>{
+    setSections(previousSections)
+    return {
+
+    }
+ }, [])
+
+  const  handleSubmit = (e)=> {s
+    e.preventDefault();
+    validateFields((err, values) => {
+      if (!err) {    
+      }
+    });
+  }
+  return (
+    <Form onSubmit={handleSubmit} >
+         <FormItem   name="section"  required  {...formItemLayout} label="School Sections">
+           {form.getFieldDecorator('section', { initialValue: "", rules: [ {required: true,message: 'Please input School Sections'}] })(
+            <Select  >
+              <Option value={""}> Select School Section</Option>
+              {
+                sections.map(x=>(
+                  <Option value={x}>{x}</Option>
+                ))
+              }
+            </Select>
+           )}
+        </FormItem>
+        <FormItem  required  {...formItemLayout} label="Term">
+           {form.getFieldDecorator('term', { initialValue: "", rules: [ {required: true,message: 'Please input Term'}] })(
+            <Select  >
+              <Option value={""}>Please Select a Term</Option>
+              <Option value={"First"}>First </Option>
+              <Option value={"Second"}>Second </Option>
+              <Option value={"Third"}>Third </Option>
+            </Select>
+           )}
+        </FormItem>
+    <FormItem {...formItemLayout} label="Admission Number">
+      {getFieldDecorator('admission', {
+        rules: [{ required: true, message: 'Please input your username!' }],
+      })(
+        <Input/>,
+      )}
+    </FormItem>
+    <FormItem  {...tailFormItemLayout}>
+          <Button disabled={loading} loading={loading} type="primary" htmlType="submit">
+          { loading?   <Icon type="loading" />   : (<> </>) }    Fetch Result
+          </Button>
+        </FormItem>
+  </Form>
+  );
+}
+ 
+PreviousResultForm.propTypes = {
+  previousSections:PropTypes.array.isRequired
+};
+ 
+export default  Form.create()(PreviousResultForm);
