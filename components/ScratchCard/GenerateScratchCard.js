76ba0b7e00e6c63b791be822d4e43fc064184a23
CodesDaniel <mailidanielifechukwu@gmail.com>
2021-04-07 01:36:52 +0100

    Changes

--- components/ScratchCard/GenerateScratchCard.js
+++ components/ScratchCard/GenerateScratchCard.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import ViewStatsScratchCard from './ViewStatsScratchCard';
 import { Row, Col } from 'antd';
-import GenerateForm from './generateForm';
+import GenerateForm from './GenerateForm';
 
  
 const GenerateScratchCard = () => {
