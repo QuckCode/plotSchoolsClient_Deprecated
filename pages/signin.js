defb9271d73f2ec54f5c9e712738c3a3186631f3
Angry Coding <mailidanielifechukwu@gmail.com>
2022-03-21 15:10:11 +0100

    SignIn Form

--- pages/signin.js
+++ pages/signin.js
@@ -1,70 +1,95 @@
-import React, { Component } from 'react';
-import Signin from '../components/Signin';
-import HomeHeader from '../components/HomeHeader';
-import {connect} from 'react-redux'
-import { useEffect } from 'react';
-import {getAllSchools} from '../redux/actions/school'
-import  Router  from 'next/router';
-import { postLogin } from '../services/restService';
-import { NotPrivateRoute } from '../components/NotPrivateRoute';
-import { loginSuccess } from '../redux/actions/auth';
- 
+import React, { Component } from "react";
+import Signin from "../components/Signin";
+import HomeHeader from "../components/HomeHeader";
+import { connect } from "react-redux";
+import { useEffect } from "react";
+import { getAllSchools } from "../redux/actions/school";
+import Router from "next/router";
+import { postLogin } from "../services/restService";
+import { NotPrivateRoute } from "../components/NotPrivateRoute";
+import { loginSuccess } from "../redux/actions/auth";
+
 class SignInPage extends Component {
    state = {
-      loading:false
-   }
-    componentDidMount(){
-       if (this.props.auth.isAuth) {
-          
+      loading: false,
+   };
+   componentDidMount() {
+      if (this.props.auth.isAuth) {
          //  Router.push('/dashboard')
-       }
-    }
-     
-    loginStaff= (regNumber, password)=>{
+      }
+   }
+
+   loginAdmin = (regNumber, password) => {
+      this.setState({ loading: true });
+      postLogin(
+         "/login/admin",
+         { regNumber, password },
+         this.props.loginSuccess
+      )
+         .then(() => {
+            this.setState({ loading: false });
+         })
+         .catch((e) => {
+            this.setState({ loading: false });
+         });
+   };
+
+   loginStaff = (regNumber, password) => {
       //  console.log({regNumber, password})
-      this.setState({loading:true})
-       postLogin("/login/staff",{regNumber, password}, this.props.loginSuccess)
-       .then(()=>{
-         this.setState({loading:false})
-      })
-      .catch(e=>{
-         this.setState({loading:false})
-      })
-    }
+      this.setState({ loading: true });
+      postLogin(
+         "/login/staff",
+         { regNumber, password },
+         this.props.loginSuccess
+      )
+         .then(() => {
+            this.setState({ loading: false });
+         })
+         .catch((e) => {
+            this.setState({ loading: false });
+         });
+   };
 
-    loginStudent= (regNumber, password)=>{
-      this.setState({loading:true})
-      postLogin("/login/student",{admissionNumber:regNumber, password}, this.props.loginSuccess)
-      .then(()=>{
-         this.setState({loading:false})
-      })
-      .catch(e=>{
-         this.setState({loading:false})
-      })
-
-    }
-   render() { 
-      const {schools, auth } = this.props
+   loginStudent = (regNumber, password) => {
+      this.setState({ loading: true });
+      postLogin(
+         "/login/student",
+         { admissionNumber: regNumber, password },
+         this.props.loginSuccess
+      )
+         .then(() => {
+            this.setState({ loading: false });
+         })
+         .catch((e) => {
+            this.setState({ loading: false });
+         });
+   };
+   render() {
+      const { schools, auth } = this.props;
       return (
          <>
-         <HomeHeader/>
-         <Signin  loading={this.state.loading} loginStaff={this.loginStaff} loginStudent={this.loginStudent} schools={schools.schools}/>
-      </>
+            <HomeHeader />
+            <Signin
+               loading={this.state.loading}
+               loginStaff={this.loginStaff}
+               loginStudent={this.loginStudent}
+               loginAdmin={this.loginAdmin}
+               schools={schools.schools}
+            />
+         </>
       );
    }
 }
- 
 
-const mapStateToProps = state => ({
+const mapStateToProps = (state) => ({
    schools: state.schools,
-   auth:state.auth
+   auth: state.auth,
 });
 
 const mapDispatchToProps = {
- loginSuccess
+   loginSuccess,
 };
 
-
-
-
-export default  NotPrivateRoute(connect(mapStateToProps, mapDispatchToProps)(SignInPage));
+export default NotPrivateRoute(
+   connect(mapStateToProps, mapDispatchToProps)(SignInPage)
+);
