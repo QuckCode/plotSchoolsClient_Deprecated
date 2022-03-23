ebce8070178fc9c0f302af97d0d7be32f360a7b7
Angry Coding <mailidanielifechukwu@gmail.com>
2022-03-23 14:30:56 +0100

    Chnaged Pricing

--- demos/mock/pricing.js
+++ demos/mock/pricing.js
@@ -1,80 +1,60 @@
-import { Activity, Gitlab, Pocket, Server } from 'react-feather';
+import { Activity, Gitlab, Pocket, Server } from "react-feather";
 
 export default [
-  {
-    icon: <Pocket size={64} strokeWidth={0.5} />,
-    title: 'Basic Licence',
-    subtitle: 'Basic account',
-    description:
-      'Perfect for  plan for new schools with less than 100 users',
-    price: 0,
-    features: [
-      {
-        title: 'Secure'
-      },
-      {
-        title: '100 user'
-      },
-      {
-        title: 'Basic '
-      }
-    ]
-  },
-  {
-    icon: <Server size={64} strokeWidth={0.5} />,
-    title: 'Basic License',
-    subtitle: 'Freelancer',
-    description:
-      'Perfect for small startups that have less than 10 team members',
-    price: 100,
-    features: [
-      {
-        title: 'Secure'
-      },
-      {
-        title: '2 users'
-      },
-      {
-        title: 'Analytics'
-      }
-    ]
-  },
-  {
-    icon: <Activity size={64} strokeWidth={0.5} />,
-    title: 'Managed License',
-    subtitle: 'Small business',
-    description:
-      'Perfect for small startups that have less than 10 team members',
-    price: 5,
-    features: [
-      {
-        title: 'Secure'
-      },
-      {
-        title: '3 users'
-      },
-      {
-        title: 'Analytics'
-      }
-    ]
-  },
-  {
-    icon: <Gitlab size={64} strokeWidth={0.5} />,
-    title: 'Extended License',
-    subtitle: 'Corporate',
-    description:
-      'Perfect for small startups that have less than 10 team members',
-    price: 10,
-    features: [
-      {
-        title: 'Secure'
-      },
-      {
-        title: 'Unlimited'
-      },
-      {
-        title: 'Analytics'
-      }
-    ]
-  }
+   {
+      icon: <Pocket size={64} strokeWidth={0.5} />,
+      title: "Basic License",
+      subtitle: "Basic account",
+      description:
+         "Perfect for  plan for new schools with less than 400 Student",
+      price: 0,
+      features: [
+         {
+            title: "400 Students",
+         },
+         {
+            title: "30 Staff ",
+         },
+         {
+            title: "Result Calculation",
+         },
+      ],
+   },
+   {
+      icon: <Server size={64} strokeWidth={0.5} />,
+      title: "Standard License",
+      subtitle: "Standard License",
+      description:
+         "Perfect for  plan for new schools with less than 2000 Student",
+      price: 100000,
+      features: [
+         {
+            title: "2000 Students",
+         },
+         {
+            title: "100 Staff",
+         },
+         {
+            title: "Fully Featured",
+         },
+      ],
+   },
+   {
+      icon: <Gitlab size={64} strokeWidth={0.5} />,
+      title: "Extended License",
+      subtitle: "Cooperate",
+      description: "Perfect for large schools ",
+      price: 200000,
+      features: [
+         {
+            title: "Unlimited Students",
+         },
+         {
+            title: "Unlimited Staffs",
+         },
+         {
+            title: "Fully Featured",
+         },
+      ],
+   },
 ];
