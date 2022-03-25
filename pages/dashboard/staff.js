33bc9b6d3d910cd50adb6d6911f890443847fffd
Angry Coding <mailidanielifechukwu@gmail.com>
2022-03-25 10:49:22 +0100

    Finished Staff Dahboard

--- pages/dashboard/staff.js
+++ pages/dashboard/staff.js
@@ -3,23 +3,47 @@ import { wrapper } from "../../redux/store";
 import { AuthToken } from "../../services/authToken";
 import { loginSuccess } from "../../redux/actions/auth";
 import { redirectError } from "../../services/redirectService";
+import StaffOverview from "../../components/StaffOverview";
 import { getSchoolsSetting } from "../../redux/actions/school";
-import StudentOverview from "../../components/StudentOverview";
-import { getStudentClassAndArmRequest } from "../../redux/actions/student";
 
-const DashboardStaffPage = ({
-   user,
-   userType,
-   oldSchoolSettings,
-   currentStudentClassAndArm,
-}) => {
-   return <> sjdjjd</>;
+const DashboardStaffPage = ({ user, subjects, classes, term, section }) => {
+   console.log(user, subjects, classes, term, section);
+   return (
+      <>
+         <StaffOverview
+            employeeDate={user.employeeDate}
+            active={user.active}
+            section={section}
+            term={term}
+            regNumber={user.regNumber}
+            classes={classes}
+            subjects={subjects}
+         />
+      </>
+   );
 };
 
 export const getServerSideProps = wrapper.getServerSideProps(async (ctx) => {
    try {
+      const store = ctx.store;
+      let data = await AuthToken.fromNext(ctx);
+      await store.dispatch(
+         loginSuccess(data.decodedToken, data.decodedToken.userType)
+      );
+      let staffId = data.decodedToken._id;
+      await store.dispatch(getSchoolsSetting(data.decodedToken.school));
+      let propStore = await store.getState();
+      return {
+         props: {
+            user: propStore.auth.user,
+            subjects: propStore.auth?.user.subjects.length,
+            classes: propStore.auth?.user.classes.length,
+            section: propStore.schools.settings?.section,
+            term: propStore.schools.settings?.term,
+         },
+      };
    } catch (error) {
-      console.log(error, ctx);
+      console.log(error);
       redirectError(ctx);
    }
 });
